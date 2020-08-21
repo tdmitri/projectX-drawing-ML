268e13da0c3723899d4f5e63bd879dfb6c67a3b0
Dmitri <dmitri.tsjornoi@gmail.com>
2020-08-21 10:11:04 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -26,9 +26,19 @@ function getColor(btn) {
     globalCompositeOperation = 'source-over';
     lineWidth = 2;
     switch (btn.getAttribute('data-color')) {
+        case "blue":
+            fillStyle = "blue";
+            break;
+
+        // case "green":
+        //     fillStyle = "green";
+        //     break;
+
         case "black":
             fillStyle = "black";
             break;
+
+
         case "eraser":
             globalCompositeOperation = 'destination-out';
             fillStyle = "rgba(0,0,0,1)";
@@ -57,10 +67,9 @@ function draw(dot) {
 }
 
 function erase() {
-    if (confirm("Want to clear")) {
+    if (confirm("Want to clear canvas ?")) {
         var ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        document.getElementById("canvasimg").style.display = "none";
     }
 }
 
@@ -72,27 +81,6 @@ function save() {
     a.href = data;
     a.download = 'image1.png';
     a.click();
-
-    canvas.style.border = "2px solid";
-    canvasimg.width = canvas.width;
-    canvasimg.height = canvas.height;
-    var ctx2 = canvasimg.getContext("2d");
-    ctx2.drawImage(canvas, 0, 0);
-    finalImg.src = canvasimg.toDataURL();
-    finalImg.style.display = "inline";
-}
-
-
-function showCoords(event) {
-    //console.log(event);
-    var x = event.clientX;
-    var y = event.clientY;
-    document.getElementById("mouse_position").innerHTML = x + ", " + y;
-}
-
-function clearCoor(event) {
-    //console.log(event);
-    document.getElementById("mouse_position").innerHTML = "";
 }
 
 function handleMouseEvent(e) {
