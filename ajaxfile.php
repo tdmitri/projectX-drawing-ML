fc7b3e9cc92169d4b6df340cd4048934f826aa5f
Alex Jermakov <alex@flox.ai>
2020-08-31 10:45:49 +0100

    demo_state

--- ajaxfile.php
+++ ajaxfile.php
@@ -1,6 +1,7 @@
 <?php 
 $image = $_POST['image'];
 $scale = $_POST['scale'];
+$unsp = $_POST['unsplash'];
 
 $location = "upload/";
 
@@ -8,8 +9,8 @@ $image_parts = explode(";base64,", $image);
 
 $image_base64 = base64_decode($image_parts[1]);
 
-$filename = "img_%01.2f.png";
-$filename = sprintf($filename, $scale);
+$filename = "img_%01.2f_%d.png";
+$filename = sprintf($filename, $scale, $unsp);
 
 
 $file = $location . $filename;
